b72a6524c1ca2e7fbc6036e138ff721826dd5b4c
vdavidmarques <vdavidmarques@gmail.com>
2025-03-17 19:15:44 -0300

    feat: solution componet

---
+++ sections/solution.php
@@ -0,0 +1,63 @@
+<section class="solution">
+    <div class="container solution--container">
+        <div class="solution--container--header">
+            <h2 class="solution--container--header--title">O que a Choran Entrega</h2>
+            <p class="solution--container--header--text">
+                Se você está investindo e não está vendo retorno, não é um problema do digital. <strong>É um problema de estratégia</strong>.
+            </p>
+        </div>
+
+        <div class="solution--container--cards">
+            <div class="solution--container--cards--card">
+                <div class="solution--container--cards--card--thumb">
+                    <img src="dist/images/escalamos-empresas.png" alt="2 mulhers loiras e um homem caucasiano conversando">
+                </div>
+                <div class="solution--container--cards--card--title">
+                   <p class="text">
+                       Escalamos empresas para múltiplos 5 dígitos/mês com previsibilidade.
+                   </p>
+                   <span class="icon"><img src="dist/icons/scale.svg" alt="ícone expandir"></span>
+                </div>
+            </div>
+
+            <div class="solution--container--cards--card">
+                <div class="solution--container--cards--card--thumb">
+                    <img src="dist/images/escalamos-empresas.png" alt="2 mulhers loiras e um homem caucasiano conversando">
+                </div>
+                <div class="solution--container--cards--card--title">
+                   <p class="text">
+                       Escalamos empresas para múltiplos 5 dígitos/mês com previsibilidade.
+                   </p>
+                   <span class="icon"><img src="dist/icons/scale.svg" alt="ícone expandir"></span>
+                </div>
+            </div>
+
+
+            <div class="solution--container--cards--card">
+                <div class="solution--container--cards--card--thumb">
+                    <img src="dist/images/escalamos-empresas.png" alt="2 mulhers loiras e um homem caucasiano conversando">
+                </div>
+                <div class="solution--container--cards--card--title">
+                   <p class="text">
+                       Escalamos empresas para múltiplos 5 dígitos/mês com previsibilidade.
+                   </p>
+                   <span class="icon"><img src="dist/icons/scale.svg" alt="ícone expandir"></span>
+                </div>
+            </div>
+
+            <div class="solution--container--cards--card">
+                <div class="solution--container--cards--card--thumb">
+                    <img src="dist/images/escalamos-empresas.png" alt="2 mulhers loiras e um homem caucasiano conversando">
+                </div>
+                <div class="solution--container--cards--card--title">
+                   <p class="text">
+                       Escalamos empresas para múltiplos 5 dígitos/mês com previsibilidade.
+                   </p>
+                   <span class="icon"><img src="dist/icons/scale.svg" alt="ícone expandir"></span>
+                </div>
+            </div>
+        </div>
+
+        <button class="solution--container--schedule-a-call">Agendar uma conversa</button>
+    </div>
+</section>
