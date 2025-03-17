58a32dea2b9e518735a49f76886e5a2e4ab518ec
vdavidmarques <vdavidmarques@gmail.com>
2025-03-17 19:14:12 -0300

    feat: form

--- index.php
+++ index.php
@@ -2,5 +2,7 @@
     include 'header.php';
     include 'sections/banner.php';
     include 'sections/sub-headline.php';
+    include 'sections/form.php';
+    include 'sections/solution.php';
     include 'footer.php';
 ?>
---
+++ sections/form.php
@@ -0,0 +1,40 @@
+<section class="form">
+    <div class="form--container container">
+        <div class="form--container--header">
+            <h3 class="title">Soluções de marketing para os mais <strong>diversos negócios</strong> de <strong>diversos tamanhos</strong></h3>
+        </div>
+
+        <div class="form--container--content">
+            <div class="form--container--content--thumb">
+                <img src="dist/images/solucoes-de-marketing-form.png" alt="Mulher caucasiana com cabelos longos loiro sorrindo segurando um celular da cor branca." srcset="">
+            </div>
+
+            <div class="form--container--content--form">
+                <h4 class="title">Preencha o formulário</h4>
+                <form action="" class="form-fields">
+                    <div class="form--container--content--form--group">
+                        <label for="name">Nome</label>
+                        <input type="text" name="name" id="name" placeholder="Digite seu nome">
+                    </div>
+                    <div class="form--container--content--form--group">
+                        <label for="email">E-mail</label>
+                        <input type="email" name="email" id="email" placeholder="Digite seu e-mail">
+                    </div>
+                    <div class="form--container--content--form--group">
+                        <label for="phone">Telefone</label>
+                        <input type="tel" name="phone" id="phone" placeholder="XX - XXXX-XXXX">
+                    </div>
+                    <div class="form--container--content--form--group">
+                        <label for="company">Segmento</label>
+                        <input type="text" name="company" id="company" placeholder="Digite o nome do seu Segmento">
+                    </div>
+                    <div class="form--container--content--form--group">
+                        <button type="submit">Receber informações</button>
+                    </div>
+
+                </form>
+            </div>
+
+        </div>
+    </div>
+</section>
