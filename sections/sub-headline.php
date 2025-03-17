95c44c0d812355aa470c3d06254362a3151676d0
vdavidmarques <vdavidmarques@gmail.com>
2025-03-17 19:15:27 -0300

    feat: sub-headline component

---
+++ sections/sub-headline.php
@@ -0,0 +1,18 @@
+<section class="subheadline">
+    <div class="subheadline--image">
+        <div class="subheadline--image--container">
+            <button class="sell-more">quero vender mais</button>
+        </div>
+    </div>
+    <div class="subheadline--content">
+        <div class="container subheadline--content--container">
+            <div class="subheadline--content--container--title">
+                <h3>Seja <strong>escalando o seu negócio</strong> ou <strong>criando um negócio digital do absoluto zero</strong>.</h3>
+            </div>
+            <div class="subheadline--content--container--description">
+                <h3 class="subtitle">Tem um PROBLEMA. Nós RESOLVEMOS.</h3>
+                <p>Se o marketing da sua empresa não gera lucro real, você não tem um negócio. </p>
+            </div>
+        </div>
+    </div>
+</section>
